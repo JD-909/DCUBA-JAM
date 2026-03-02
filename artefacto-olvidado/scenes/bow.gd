affc3f2bdc14608e4cddbd202fd90d35b9b3a11f
909 <Horus@TheNyle.com>
2026-03-02 19:38:30 -0300

    el bow deja de disparar si esta borrado

--- artefacto-olvidado/scenes/bow.gd
+++ artefacto-olvidado/scenes/bow.gd
@@ -3,14 +3,15 @@ extends PermanentDrawnBody
 @onready var arrow_res : Resource = load("res://scenes/arrow.tscn")
 @onready var shoot_sound : AudioStreamPlayer2D = $ShootSound
 @onready var arrow_parent = $"../../Spikes"
+@onready var timer : Timer = $Timer
 
 @export var direction : Vector2 = Vector2(1,0)
 
 func when_drawn() -> void:
-	pass
+	timer.start()
 
 func when_erased() -> void:
-	pass
+	timer.stop()
 
 func _on_timer_timeout() -> void:
 	shoot()

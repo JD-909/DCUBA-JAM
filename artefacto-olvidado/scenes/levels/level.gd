f7a59f169b4b0e77583aa354abd7a2d6122cb4f2
909 <Horus@TheNyle.com>
2026-03-05 18:46:24 -0300

    agrego var last_level_visited

--- artefacto-olvidado/scenes/levels/level.gd
+++ artefacto-olvidado/scenes/levels/level.gd
@@ -1,8 +1,11 @@
 class_name Level
 extends Node2D
 
+@export var level_ID : int = 0
+
 func _ready() -> void:
 	$AudioStreamPlayer2D.play()
+	Global.visit(level_ID)
 
 func restart_level() -> void:
 	# Reload the level

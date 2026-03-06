73bb1d8cd8631228cbaa6a3eadb1d87753c84e4f
909 <Horus@TheNyle.com>
2026-03-06 12:58:50 -0300

    agrego level completed arrays

--- artefacto-olvidado/globals/global.gd
+++ artefacto-olvidado/globals/global.gd
@@ -1,6 +1,11 @@
 extends Node
 
 var last_level_visited : int 
+var level_done : Array[bool] = [false,false,false,false]
+var level_done_draw_limit : Array[bool] = [false,false,false,false]
+var level_done_erase_limit : Array[bool] = [false,false,false,false]
+var level_done_erase_all : Array[bool] = [false,false,false,false]
+var level_done_secret : Array[bool] = [false,false,false,false]
 
 func visit(level_ID : int) -> void:
 	last_level_visited = level_ID

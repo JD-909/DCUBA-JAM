af2efd7d4d2045040f772873c6609f80d44b4217
909 <Horus@TheNyle.com>
2026-03-11 16:01:35 -0300

    agrego plataforma en movimiento con pausa

---
+++ artefacto-olvidado/scenes/movement_cog.gd
@@ -0,0 +1,10 @@
+extends PermanentDrawnBody
+
+func _process(_delta: float) -> void:
+	rotate(0.1)
+
+func when_drawn() -> void:
+	pass
+
+func when_erased() -> void:
+	pass
--- artefacto-olvidado/scenes/moving_platform.gd
+++ artefacto-olvidado/scenes/moving_platform.gd
@@ -1,4 +1,4 @@
-extends StaticBody2D
+extends CharacterBody2D
 
 const SPEED = 100
 
@@ -7,10 +7,14 @@ enum Axis_Direction {Horizontal, Vertical}
 @export var Axis : Axis_Direction = Axis_Direction.Horizontal
 @export var tilesToTravel : int = 1
 
+@onready var timer : Timer =$Timer
+
 var movementVector : Vector2
+var initialMovementVector : Vector2
 var InitialPosition : Vector2
 var FinalPosition : Vector2
 var dir = 1
+var speed = 100
 
 func _ready() -> void:
 	
@@ -18,20 +22,39 @@ func _ready() -> void:
 	
 	match Axis:
 		Axis_Direction.Horizontal:
-			movementVector = Vector2(SPEED,0)
+			initialMovementVector = Vector2(speed,0)
+			movementVector = initialMovementVector
 			FinalPosition = InitialPosition + Vector2(tilesToTravel*64, 0)
 		Axis_Direction.Vertical:
+			initialMovementVector = Vector2(0,speed)
+			movementVector = initialMovementVector
 			FinalPosition = InitialPosition + Vector2(0, tilesToTravel*64)
-			movementVector = Vector2(0,SPEED)
 	
 	print(InitialPosition)
 	print(FinalPosition)
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	
 	if global_position <= InitialPosition:
-		dir = 1
+		if dir == -1:
+			change_dir()
 	elif global_position >= FinalPosition:
-		dir = -1
+		if dir == 1:
+			change_dir()
 	
-	move_and_collide(movementVector*delta*dir)
+	velocity = movementVector*dir
+	move_and_slide()
+
+func change_dir() -> void:
+	stop_movement()
+	timer.start()
+	dir = -dir
+
+func _on_timer_timeout() -> void:
+	resume_movement()
+
+func stop_movement() -> void:
+	movementVector = Vector2.ZERO
+
+func resume_movement() -> void:
+	movementVector = initialMovementVector

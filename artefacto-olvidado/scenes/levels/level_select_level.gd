08dbe1fd52026f64c401a56be82c1dda800d07c6
909 <Horus@TheNyle.com>
2026-03-16 13:48:34 -0300

    agrego piso para world select level y level select 2

--- artefacto-olvidado/scenes/levels/level_select_level.gd
+++ artefacto-olvidado/scenes/levels/level_select_level.gd
@@ -1,5 +1,7 @@
 extends Level
 
+@export var world : int 
+
 @onready var player_position_array : Array[Vector2] = [
 	$PermanentDrawnBodies/ExitDoor.position,
 	$PermanentDrawnBodies/DoorLevel1.position,
@@ -11,5 +13,6 @@ extends Level
 
 func _ready() -> void:
 	$AudioStreamPlayer2D.play()
-	$Drawer.position = player_position_array[Global.last_level_visited]
+	# world -1 *5 es para que los niveles mas altos den un numero entre 0 y 5 
+	$Drawer.position = player_position_array[Global.last_level_visited - ((world -1) * 5)]
 	Global.visit(level_ID)

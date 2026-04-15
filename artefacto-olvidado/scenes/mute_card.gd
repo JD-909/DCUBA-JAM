884922a4fd3970c25582d449b4d6a56143cab574
909 <Horus@TheNyle.com>
2026-04-15 17:42:03 -0300

    agrego mute music y mute Bow

--- artefacto-olvidado/scenes/mute_card.gd
+++ artefacto-olvidado/scenes/mute_card.gd
@@ -1,8 +1,10 @@
 class_name MuteCard
 extends PermanentDrawnBody
 
+@export var bus_mute : int
+
 func when_drawn() -> void:
-	AudioServer.set_bus_mute(1, true)
+	AudioServer.set_bus_mute(bus_mute, true)
 
 func when_erased() -> void:
-	AudioServer.set_bus_mute(1, false)
+	AudioServer.set_bus_mute(bus_mute, false)
